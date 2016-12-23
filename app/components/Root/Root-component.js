c161fd9f453602ae55c3b512d76a4e505f30962d
milocosmopolitan <milocosmopolitan@gmail.com>
2016-12-23 16:38:04 -0500

    responsive

--- app/components/Root/Root-component.js
+++ app/components/Root/Root-component.js
@@ -6,6 +6,13 @@
 
 import React, { Component } from 'react';
 import { connect } from'react-redux';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { 
+		Link, 
+		Element, 
+		Events, 
+		animateScroll, 
+		scrollSpy } from 'react-scroll';
 
 /* -----------------    COMPONENT     ------------------ */
 
@@ -13,13 +20,94 @@ class RootComponent extends Component {
 	constructor(props) {
 		super(props);		
 	}
+	componentDidMount() {
 
+    Events.scrollEvent.register('begin', function(to, element) {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register('end', function(to, element) {
+      console.log("end", arguments);
+    });
+
+    scrollSpy.update();
+
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  scrollToTop() {
+    animateScroll.scrollToTop();
+  }
+  scrollToBottom() {
+    animateScroll.scrollToBottom();
+  }
+  scrollTo() {
+    animateScroll.scrollTo(100);
+  }
+  scrollMore() {
+    animateScroll.scrollMore(100);
+  }
+  handleSetActive(to) {
+    console.log(to);
+  }
 	render() {
     
 		return (
-			<div className="container">
-			Root
-			</div>
+				<div>
+        <Link activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={this.handleSetActive}>
+          Test 1
+        </Link>
+        <Link activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} delay={1000}>
+          Test 2 (delay)
+        </Link>
+        <Link className="test6" to="anchor" spy={true} smooth={true} duration={500}>
+          Test 6 (anchor)
+        </Link>  
+
+        <Element name="test1" className="element">
+          test 1
+        </Element>
+
+        <Element name="test2" className="element">
+          test 2
+        </Element>
+
+        <div id="anchor" className="element">
+          test 6 (anchor)
+        </div>
+
+
+        <Link to="firstInsideContainer" containerId="containerElement">
+          Go to first element inside container
+        </Link>
+
+        <Link to="secondInsideContainer" containerId="containerElement">
+          Go to second element inside container
+        </Link>
+        <div className="element" id="containerElement">
+          <Element name="firstInsideContainer">
+            first element inside container
+          </Element>
+
+          <Element name="secondInsideContainer">
+            second element inside container
+          </Element>
+        </div>
+
+        <a onClick={this.scrollToTop}>To the top!</a>
+        <br/>
+        <a onClick={this.scrollToBottom}>To the bottom!</a>
+        <br/>
+        <a onClick={this.scrollTo}>Scroll to 100px from the top</a>
+        <br/>
+        <a onClick={this.scrollMore}>Scroll 100px more from the current position!</a>
+      </div>
+				
+			
 		);
 	}
 
